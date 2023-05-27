41f2c215f7322be5ddddc7a04b90c0f9d1822ad9
Elmurod Vokhidov <elmurodvkh@gmail.com>
2023-05-28 00:09:29 +0500

    complate react  functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Cart from "./components/Cart";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
       <Cart />
     </div>
   );
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,24 +1,39 @@
 import { useEffect, useState } from "react";
 import React from 'react';
 import { GrCart } from "react-icons/gr";
+import { toast } from "react-toastify";
 
 function Cart(props) {
     const [main, setMain] = useState([]);
+    const [cart, setCart] = useState([]);
+    const [modal, setModal] = useState(false);
 
+    // Add to cart handler function
+    const addCartHandler = (item) => {
+        setCart((prev) => {
+            return [...prev, item]
+        });
+        toast.dark("Product added successfully!")
+    };
+
+    // Open modal function
+    const modalHandler = () => {
+        setModal(!modal);
+    };
+
+    // Get the product from API
     useEffect(() => {
         fetch('https://fakestoreapi.com/products/')
             .then(res => res.json())
             .then(json => setMain(json))
     }, []);
 
-    console.log(main);
-
     return (
         <div className="album py-5 bg-light">
             <div className="container">
                 <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                     {main.map((item) => (
-                        <div className="col">
+                        <div key={item.id} className="col">
                             <div className="card shadow-sm p-3" style={{ minHeight: '550px' }}>
                                 <div className="card-title">
                                     <h4 className="text-muted text-center">Product #{item.id}</h4>
@@ -32,22 +47,58 @@ function Cart(props) {
                                     <div>
                                         <span>{item.category}</span>
                                     </div>
-                                    <span className="text-muted">{item.price}</span>
+                                    <span className="text-muted">${item.price}</span>
                                 </div>
+                                <button onClick={() => addCartHandler(item)} className="mt-3 btn btn-outline-primary">Add Cart</button>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
             <div className="fixed-top m-3">
-                <button type="button" class="btn btn-primary position-relative">
+                <button onClick={modalHandler} type="button" className="btn btn-primary position-relative">
                     <span><GrCart /></span>
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                        99+
-                        <span class="visually-hidden">unread messages</span>
+                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        {cart.length}
+                        <span className="visually-hidden">unread messages</span>
                     </span>
                 </button>
             </div>
+            {
+                modal && (<div className="modal" style={{ display: "block", background: "rgba(0, 0, 0, .8)" }}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Cart</h5>
+                                <button onClick={modalHandler} type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                {cart.map((item, index) => (
+                                    <div key={index} className="card mb-3">
+                                        <div className="row g-0">
+                                            <div className="col-md-4">
+                                                <img src={item.image} alt={item.title} className="img-fluid rounded-start" />
+                                            </div>
+                                            <div className="col-md-8">
+                                                <div className="card-body">
+                                                    <h5 className="card-title">{item.title}</h5>
+                                                    <p className="cart-text text-muted">{item.description.slice(0, 100)}</p>
+                                                    <p className="card-text">
+                                                        <small className="text-muted">${item.price}</small>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="modal-footer">
+                                <button onClick={modalHandler} type="button" className="btn btn-primary">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                )}
         </div>
     );
 }
